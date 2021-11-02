b867ef564b1af4850ee54f6100fe7f2528570911
Arthur262000 <arthur262000@gmail.com>
2021-11-02 18:16:02 +0100

    Commit

--- monapp/src/App.js
+++ monapp/src/App.js
@@ -1,25 +1,70 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class Profile extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      profils : [
+        {
+          prenom: "Bob",
+          nom: "Smith",
+          date: 1992
+        },
+        {
+          prenom: "Martine",
+          nom: "Neil",
+          date: 1982
+        },
+        {
+          prenom: "Camille",
+          nom: "Dacosta",
+          date: 1995
+        }
+      ]
+      
+    };
+
+
+    this.handleClick = this.handleClick.bind(this);
+
+  }
+
+  handleClick = () =>{
+    this.setState({
+      profils : [
+        {
+          prenom: "Thierry",
+          nom: "Smith",
+          date: 1992
+        },
+        {
+          prenom: "Martine",
+          nom: "Neil",
+          date: 1982
+        },
+        {
+          prenom: "Camille",
+          nom: "Dacosta",
+          date: 1995
+        }
+      ]
+    });
+  }
+
+
+  render() {
+    return (
+      <div>
+        <button className="btn btn-primary" onClick={this.handleClick}>Bob</button>
+        <button className="btn btn-primary" onClick>Martine</button>
+        <button className="btn btn-primary" onClick>Camille</button>
+        {this.state.profils[0].prenom}
+        </div>
+    );
+  }
 }
 
-export default App;
+
+
+export default Profile;
